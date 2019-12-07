7668944c070c734c3789c43f070bbb94caf22307
Ana Arribas <arribas.ibd@gmail.com>
2019-12-07 19:57:17 +0100

    array fetch

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('>> Ready :)');
+const button = document.querySelector('#button');
+const input4 = document.querySelector('#radio4');
+const input6 = document.querySelector('#radio6');
+const input8 = document.querySelector('#radio8');
+const allinputs = document.querySelectorAll('.input');
+
+
+const connectHandler = () => {
+    for (let eachinput of allinputs)
+        fetch(`https://beta.adalab.es/ejercicios-extra/api/pokemon-cards/${eachinput.value}.json`)
+            .then(response => response.json())
+            .then(data => displayShows(data))
+};
+
+
+const displayShows = patata => {
+    console.log(patata);
+}
+
+
+
+button.addEventListener('click', connectHandler);
